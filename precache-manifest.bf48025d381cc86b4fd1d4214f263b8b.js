4f8821d9f23f90bcd5137dd123021d53d4749575
Vlad Bond <foolowin@gmail.com>
2020-11-23 15:29:36 +0300

    Updates

---
+++ precache-manifest.bf48025d381cc86b4fd1d4214f263b8b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "14096276cc6d71552308",
+    "url": "/server-react-app/static/css/main.9bc6bcd2.chunk.css"
+  },
+  {
+    "revision": "14096276cc6d71552308",
+    "url": "/server-react-app/static/js/main.14096276.chunk.js"
+  },
+  {
+    "revision": "3d01e587d51db1aafcba",
+    "url": "/server-react-app/static/css/1.cff51a6f.chunk.css"
+  },
+  {
+    "revision": "3d01e587d51db1aafcba",
+    "url": "/server-react-app/static/js/1.3d01e587.chunk.js"
+  },
+  {
+    "revision": "5f99a07f841b4f74a5e7",
+    "url": "/server-react-app/static/js/runtime~main.5f99a07f.js"
+  },
+  {
+    "revision": "4da810837c6dd45262462cccf9566c34",
+    "url": "/server-react-app/static/media/got.4da81083.jpeg"
+  },
+  {
+    "revision": "d35a52e4a0bb5bfdb19f325eba820ad8",
+    "url": "/server-react-app/index.html"
+  }
+];
